2e45dcd9ce5d4e87fabb32e99c4ae2d10d73039e
Alex McKenzie <alex.mckenzie@gmail.com>
2020-03-09 16:29:35 +1100

    Added ability to continue an import from when it was last run.

--- app/Console/Commands/ImportData.php
+++ app/Console/Commands/ImportData.php
@@ -47,7 +47,48 @@ class ImportData extends Command
             $this->error("No stockpile folder.");
         }
 
+        $max_date = null;
+        $choice = 3;
+
+        $max_id = DB::table('speeds')->max('id');
+
+        // Check speeds table is empty
+        if ($max_id > 0) {
+
+            $answer = $this->choice('There are existing speeds. What would you like to do? 🤔', [1 => 'Continue the import', 2 => 'Start a fresh import', 3 => 'Quit']);
+
+            switch ($answer) {
+                case 'Continue the import':
+                    $choice = 1;
+                    $max_date = DB::table('speeds')->max('created_at');
+                    break;
+                case 'Start a fresh import':
+                    $choice = 2;
+
+                    if (! $this->confirm('Are you sure you want to start a new import?')) {
+                        $choice = 3;
+                    }
+
+                    break;
+                case 'Quit':
+                    $choice = 3;
+            }
+        }
+
+        // Quit if requested
+        if ($choice == 3) {
+            $this->exit();
+        }
+
+        // Empty speeds table if requested
+        if ($choice == 2) {
+            $this->info("Clearing speeds table. 🗑");
+            DB::table("speeds")->truncate();
+        }
+
         $data_files = glob($data_location . '/*.{txt}', GLOB_BRACE);
+
+        $data_files = $this->filterDataFiles($data_files, $max_date);
 
         $bar = $this->output->createProgressBar(count($data_files));
         $bar->start();
@@ -57,17 +98,17 @@ class ImportData extends Command
             $handle = fopen($file, "r");
             if ($handle) {
                 while (($line = fgets($handle)) !== false) {
-                    // process the line read.
-                    $data = explode(',', $line);
-                    $date = $this->getDateString(basename($file));
 
-                    $asn = Asn::whereName(str_replace('_', ' ', $data[0]))->first();
+                    $data = explode(',', $line);
+                    $date = $this->getDateString($file);
+
+                    $asn = Asn::whereName($this->getRealName($data[0]))->first();
 
                     if (empty($asn)) {
                         $asn = new Asn([
-                            'name' => str_replace('_', ' ', $data[0]),
+                            'name' => $this->getRealName($data[0]),
                             'country' => 'AUS',
-                            'color' => '#' . str_pad(dechex(rand(0x000000, 0xFFFFFF)), 6, 0, STR_PAD_LEFT),
+                            'color' => $this->getColor($data[0]),
                             'created_at' => Carbon::now(),
                             'updated_at' => Carbon::now()
                         ]);
@@ -85,33 +126,81 @@ class ImportData extends Command
 
                     $speed->save();
 
-                    /*
-                     * DB::table('ans')->insertOrIgnore([
-                        ['name' => $data[0], 'country' => 'AUS'],
-                    ]);
-                     */
-
                 }
 
                 fclose($handle);
             } else {
-                $this->error("Error opening {$file}.");
+                $this->error("Error opening {$file}. ☹️");
             }
 
             $bar->advance();
         }
 
         $bar->finish();
+        $this->info(PHP_EOL);
+        $this->info("Finished importing data. 🥳");
+    }
 
-        $this->info("Finished importing data.");
+    protected function getRealName($name) {
+        return str_replace('_', ' ', $name);
     }
 
     protected function getDateString($filename)
     {
-        $string = str_replace('.txt', '', $filename);
+        $string = str_replace('.txt', '', basename($filename));
 
         $string_array = explode('-', $string);
 
         return Carbon::parse($string_array[2] . '-' . $string_array[0] . '-' . $string_array[1])->format('Y-m-d');
+    }
+
+    protected function getColor($name) {
+
+        $name = $this->getRealName($name);
+
+        $colors = [
+            'Aussie Broadband' => '#5ea429',
+            'Dodo' => '#33034F',
+            'Exetel' => '#009f4d',
+            'iiNet Limited' => '#ff8200',
+            'iPrimus' => '#35cc99',
+            'MyRepublic' => '#9d3192',
+            'Optus' => '#fdcc08',
+            'Telstra Internet' => '#2c74d3',
+            'TPG Internet' => '#561666',
+            'Vodafone Australia' => '#E60000',
+        ];
+
+        if (array_key_exists($name, $colors)) {
+            return $colors[$name];
+        } else {
+            return '#' . str_pad(dechex(rand(0x000000, 0xFFFFFF)), 6, 0, STR_PAD_LEFT);
+        }
+    }
+
+    protected function filterDataFiles($files, $max_date) {
+
+        if (is_null($max_date)) {
+            return $files;
+        }
+
+        $files_array = [];
+        foreach($files as $file) {
+            if (Carbon::parse($this->getDateString($file))->gt(Carbon::parse($max_date))) {
+                $files_array[] = $file;
+            }
+        }
+
+        if (count($files_array) > 0) {
+            return $files_array;
+        }
+
+        $this->info("No recent files to import.");
+        $this->exit();
+    }
+
+    protected function exit() {
+        $this->info("Bye! 👋");
+        exit();
     }
 }
